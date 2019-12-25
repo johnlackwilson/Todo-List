b5237d457cbf14835fd72bc5fe994afd80b26ac7
John Lack-Wilson <johnlackwilson@icloud.com>
2019-12-25 01:24:33 +0000

    - Added a welcome message - Added a prompt - Refactored menu items out to their own functions - Implemented a way for the user to supply menu args at the same time as the command or after - Improved readability of the code - Fixed issue #17 - now program waits for user to enter a title for their list item.

--- src/main/java/com/johnlackwilson/App.java
+++ src/main/java/com/johnlackwilson/App.java
@@ -1,24 +1,31 @@
 package main.java.com.johnlackwilson;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
- * The main app class to interact as the CLI with the user.
+ * <p>The main app class to interact as the CLI with the user.</p>
  */
 public class App {
 
+    private static final String APP_TITLE = "==================================\n" +
+                                            "     ______        ____           \n" +
+                                            "    /_  __/____   / __ \\ ____    \n" +
+                                            "     / /  / __ \\ / / / // __ \\  \n" +
+                                            "    / /  / /_/ // /_/ // /_/ /    \n" +
+                                            "   /_/   \\____//_____/ \\____/   \n" +
+                                            "                                  \n" +
+                                            "==================================\n";
+
     private static final String COMMAND_NOT_RECOGNISED_ERR = "Command not recognised.";
+    private static final String PROMPT = "$";
 
     /**
-     * The Scanner to ask for user input.
+     * <p>The Scanner to ask for user input.</p>
      */
     private Scanner input;
 
     /**
-     * The connector to interface with the apps' database.
+     * <p>The connector to interface with the apps' database.</p>
      */
     private DBConnector connector;
 
@@ -45,10 +52,93 @@ public class App {
     }
 
     /**
+     * <p>Prints the welcome message.</p>
+     */
+    private void welcomeMessage() {
+        System.out.printf("%s\n", APP_TITLE);
+    }
+
+    /**
+     * <p>Clears the console by sending ANSI escape codes.</p>
+     */
+    private void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    /**
      * <p>Prints the menu to the console.</p>
      */
     private void printMenu() {
-        Arrays.asList(Menu.values()).forEach(item -> System.out.println("[" + item.getMenuItemName() + "]: " + item.getMenuItemDesc()));
+        welcomeMessage();
+        Arrays.asList(Menu.values()).forEach(item -> System.out.printf("%-5s- %s\n", item.getMenuItemName(), item.getMenuItemDesc()));
+    }
+
+    /**
+     * <p>Prints the menu.</p>
+     */
+    private void help() {
+        clearScreen();
+        printMenu();
+    }
+
+    /**
+     * <p>Prints all items.</p>
+     */
+    private void list() {
+        clearScreen();
+        List<TodoItem> todoItems = this.connector.getAll();
+        for(TodoItem item: todoItems) {
+            System.out.printf("%-4s %s\n", ("[" + item.getId() + "]"), item.getTitle());
+        }
+    }
+
+    /**
+     * <p></p>
+     */
+    private void add(List<String> args) {
+        clearScreen();
+        String title = null;
+
+        // If the list is empty then ask the user for their title.
+        if(args.isEmpty()) {
+            System.out.printf("%s Title: ", PROMPT);
+            title = input.nextLine().trim();
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for(String arg: args) {
+                sb.append(arg);
+                sb.append(" ");
+            }
+            title = sb.toString().trim();
+        }
+
+        this.connector.add(title);
+    }
+
+    /**
+     * <p></p>
+     */
+    private void delete(List<String> args) {
+        clearScreen();
+        String itemNumString;
+
+        // If user supplied args then assume the first is the number of the item to delete.
+        if(!args.isEmpty()) {
+            itemNumString = args.get(0);
+        } else {
+            System.out.printf("%s Item number: ", PROMPT);
+            itemNumString = input.nextLine();
+        }
+
+        try {
+            int itemNum = Integer.parseInt(itemNumString);
+            this.connector.delete(itemNum);
+        } catch (InputMismatchException e) {
+            System.err.printf("%s ERROR: Item number required.\n", PROMPT);
+        } catch (NumberFormatException e) {
+            System.err.println("ERROR: Item number required.");
+        }
     }
 
     /**
@@ -56,45 +146,56 @@ public class App {
      * @throws IllegalArgumentException if user inputs an invalid command.
      */
     private void menuHandler() throws IllegalArgumentException {
-        String choice = this.input.next();
+        System.out.printf("\n%s ", PROMPT);
+        String[] userInput = this.input.nextLine().trim().split("\\s+");
 
-        // Unfortunately cannot use a switch statement to check what user wants to do
-        // because enum does not produce a constant expression which the switch needs.
+        // Make sure nothing funny is going on with the split array.
+        if(userInput.length < 1) {
+            return;
+        }
+        String command = userInput[0];
 
         // Check if user wants to exit.
-        if(choice.equals(Menu.EXIT.getMenuItemName())) {
+        if(command.equals(Menu.EXIT.getMenuItemName())) {
             System.out.println("Bye!");
             System.exit(0);
         }
 
         // Check for valid command.
-        if(!menuContains(choice)) {
+        if(!menuContains(command)) {
             throw new IllegalArgumentException();
         }
 
-        // Now we know that we have a valid command so let's handle it.
-        if(choice.equals(Menu.HELP.getMenuItemName())) {
-            printMenu();
-        } else if(choice.equals(Menu.LIST.getMenuItemName())) {
-            List<Todo> todoItems = this.connector.getAll();
-            for(Todo item: todoItems) {
-                System.out.println(String.format("[%d]: %s", item.getId(), item.getTitle()));
-            }
-        } else if(choice.equals(Menu.ADD.getMenuItemName())) {
-            System.out.print("[TITLE] ");
-            this.connector.add(input.nextLine().trim());
-        } else if(choice.equals(Menu.DEL.getMenuItemName())) {
-            System.out.print("[ITEM NUMBER] ");
-            try {
-                this.connector.delete(input.nextInt());
-            } catch (InputMismatchException e) {
-                System.err.println("[ERROR] Item number required.");
-            }
+        // Get any args the user has passed in
+        List<String> userArgs = new ArrayList<>();
+        if(userInput.length > 1) {
+            userArgs.addAll(Arrays.asList(userInput).subList(1, userInput.length));
+        }
+
+        // Unfortunately cannot use a switch statement to check what user wants to do
+        // because enum does not produce a constant expression which the switch needs.
+
+        // HELP
+        if(command.equals(Menu.HELP.getMenuItemName())) {
+            help();
+        }
+        // LIST
+        else if(command.equals(Menu.LIST.getMenuItemName())) {
+            list();
+        }
+        // ADD
+        else if(command.equals(Menu.ADD.getMenuItemName())) {
+            add(userArgs);
+        }
+        // DELETE
+        else if(command.equals(Menu.DEL.getMenuItemName())) {
+            delete(userArgs);
         }
     }
 
     public static void main(String[] args) {
         App app = new App();
+        app.clearScreen();
         app.printMenu();
 
         while(true) {
